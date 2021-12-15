ad4e1065e533a3bfd4a65ce1feb6dc2013b17493
hwalanlee <hwalan.lee@bespinglobal.com>
2021-12-15 14:13:12 +0900

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 8080;
 
 app.get('/', function (req, res) {
-    res.send('Hello World from second test!!')
+    res.send('Hello World from second test!')
 });
 
 app.listen(port, function () {
